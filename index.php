9c70405c62c6eede5775edd2e8c0a59bce1d5266
Over Zada <henrikki.4114@gmail.com>
2022-08-18 22:51:31 -0300

    criando a classe page

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
