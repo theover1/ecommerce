fc4a6e91597e71869315a16f4008729c355abeb0
Over Zada <henrikki.4114@gmail.com>
2022-08-18 23:58:33 -0300

    criando o page admin e tpl

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,16 +9,15 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [ 
-		
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()) {
+	public function __construct($opts = array(), $tpl_dir = "/views/") {
 
 		$this->options = array_merge($this->defaults, $opts);
 		
 		$config = array(
-			"tpl_dir"  =>$_SERVER["DOCUMENT_ROOT"]."/views/",
+			"tpl_dir"  =>$_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 			"cache_dir" => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 			"debug"	=> false
 		);
@@ -31,14 +30,8 @@ class Page {
 		
 		$this->tpl->draw("header");
 
-		}
+	}
 
-		public function __destruct()
-		{
-
-		$this->tpl->draw("footer");
-
-		}
 
 	private function setData($data = array())
 	{
@@ -59,7 +52,12 @@ class Page {
 	}
 
 
-	
+	public function __destruct()
+	{
+
+		$this->tpl->draw("footer");
+
+	}
 }
 
 ?>
